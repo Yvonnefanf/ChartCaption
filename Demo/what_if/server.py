12f1a3b0b7db146070bacf5f69ff03412314d020
Yvonnefanf <m18522608197@163.com>
2024-06-07 23:35:45 +0800

    fix

--- Demo/what_if/server.py
+++ Demo/what_if/server.py
@@ -225,4 +225,4 @@ def similar_retrival(instance, instances_0,instances_1):
   
   
 if __name__ == '__main__':
-    app.run(debug=True, port=3333)
+    app.run(debug=False, host='0.0.0.0',port=3333)
